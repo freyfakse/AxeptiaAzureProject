7f7f2510031b256e081c4d9e87f1ba5dea119a6c
Jonas Claesson <jonascl@online.no>
2020-05-30 13:30:36 +0200

    initial commit

---
+++ BlobTriggerCSharpTxt2Json.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Axeptia.Function
+{
+    public static class BlobTriggerCSharpTxt2Json
+    {
+        [FunctionName("BlobTriggerCSharpTxt2Json")]
+        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            
+            Txt2JsonConverter MyConverter = new Txt2JsonConverter();
+
+            MyConverter.TxtToList();
+        }
+    }
+}
---
+++ Txt2JsonConverter.cs
@@ -0,0 +1,19 @@
+namespace Axeptia.Function
+{
+    public class Txt2JsonConverter
+    {
+        public Txt2JsonConverter()
+        {
+
+        }
+
+        public void TxtToList()
+        {
+
+        }
+
+        
+    }
+
+    
+}
