8e76b5eab4f389ce6af6d32175404d62b69740f7
Jonas Claesson <jonascl@online.no>
2020-05-31 16:15:23 +0200

    added LineItem class, relocated code to main, added .txt file to List<LineItem> functionality.

--- BlobTriggerCSharpTxt2Json.cs
+++ BlobTriggerCSharpTxt2Json.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -10,13 +12,38 @@ namespace Axeptia.Function
     public static class BlobTriggerCSharpTxt2Json
     {
         [FunctionName("BlobTriggerCSharpTxt2Json")]
-        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-            Txt2JsonConverter MyConverter = new Txt2JsonConverter();
 
-            MyConverter.TxtToLists("fornavn;etternavn;tittel;hans;gretesen;kokk");
+
+            //TxtToJsonConverter MyConverter = new TxtToJsonConverter();
+
+            //MyConverter.TxtToList("fornavn;etternavn;tittel;hans;gretesen;kokk");
+
+
+
+
+            List<LineItem> itemList = null;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"E:\Backup\Github\AxeptiaAzureProject\personnel.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                List<string> lineList = line.Split(";").ToList();
+
+                int i = 0;
+
+                itemList[i].firstName = lineList[0];
+                itemList[i].lastName = lineList[1];
+                itemList[i].title = lineList[2];
+
+                i++;
+            }
+            itemList.RemoveAt(0);//remove header element
+
+            file.Close();
         }
     }
 }
---
+++ LineItem.cs
@@ -0,0 +1,14 @@
+namespace Axeptia.Function
+{
+    public class LineItem
+    {
+        public string firstName;
+        public string lastName;
+        public string title; 
+        public LineItem()
+        {
+
+        }
+        
+    }
+}
