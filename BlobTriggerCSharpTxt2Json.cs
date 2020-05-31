7215ed45fa5e92a4b704f17d6add35229ea44468
Jonas Claesson <jonascl@online.no>
2020-05-31 12:30:52 +0200

    added text to lists function

--- BlobTriggerCSharpTxt2Json.cs
+++ BlobTriggerCSharpTxt2Json.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
+
 namespace Axeptia.Function
 {
     public static class BlobTriggerCSharpTxt2Json
@@ -12,10 +13,10 @@ namespace Axeptia.Function
         public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-            
+
             Txt2JsonConverter MyConverter = new Txt2JsonConverter();
 
-            MyConverter.TxtToList();
+            MyConverter.TxtToLists("fornavn;etternavn;tittel;hans;gretesen;kokk");
         }
     }
 }
